12de474317ff53b68df8072309a1bc134ab33ccb
Daniel Eissing <daniel.eissing@sinnerschrader.com>
2015-03-18 21:04:47 +0100

    Adds standalone alternative for initializing pouchdb-store.

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 /* jshint node:true */
 'use strict';
 
-var ShelfDb = require('./lib/shelfdb.js');
+var PouchDbStore = require('./lib/shelfdb.js');
+
+var PouchDb;
+
+var stores = {};
 
 module.exports = {
-  collection: function (options) {
-    return ShelfDb.load(this, options);
+  //
+  // Plugin loader for PouchDbStore.
+  //
+  // Usage:
+  //   var PouchDb = require('pouchdb');
+  //   PouchDb.plugin(require('pouchdb-store'));
+  //   var MyStore = new PouchDb('myStore').store();
+  //   ...
+  //
+  store: function (options) {
+    return PouchDbStore.load(this, options);
+  },
+
+  //
+  // Standalone loader for PouchDbStore.
+  //
+  // Usage:
+  //   var PouchDbStore = require('pouchdb-store');
+  //   var MyStore = PouchDbStore.open('myStore');
+  //   ...
+  //
+  open: function (dbName, options) {
+
+    if (!PouchDb) {
+      PouchDb = require('pouchdb');
+      PouchDb.plugin(PouchDbStore);
+    }
+
+    if (!stores[dbName]) {
+      var pouch = new PouchDb(dbName, options);
+      stores[dbName] = PouchDbStore.load(pouch, options);
+    }
+
+    return stores[dbName];
   }
 };
