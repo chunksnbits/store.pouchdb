c99538c1dda41a7651dfb97eb09ed52daacf07e8
Daniel Eissing <daniel.eissing@sinnerschrader.com>
2015-03-22 21:27:16 +0100

    Fixes defaults with db creation on sync and server options.

--- test/sync.test.js
+++ test/sync.test.js
@@ -1,5 +1,5 @@
 /* jshint node:true */
-/* global describe, afterEach, beforeEach, it, catchIt */
+/* global describe, afterEach, before, beforeEach, it */
 
 // Ignores "Expected an assignment or function call and instead saw an expression"
 /* jshint -W030 */
@@ -12,11 +12,21 @@ var PouchDbStore = require('../index');
 var expect = require('chai').expect;
 var http = require('http');
 
-var pouch;
+var pouch, syncStore;
 
 PouchDb.plugin(PouchDbStore);
 
 describe('Testing shelfdb sync', function () {
+
+  before(function () {
+    var express = require('express');
+    var app = express();
+
+    syncStore = new PouchDb('sync', { db: require('memdown') }).store();
+    syncStore.listen(app, { root: '/remote' });
+
+    app.listen(1234);
+  });
 
   beforeEach(function () {
     pouch = new PouchDb('test', { db: require('memdown') });
@@ -24,11 +34,39 @@ describe('Testing shelfdb sync', function () {
 
   describe('using sync', function () {
 
-    it('will create a sync connection when applying sync() for this store',
+    it('will create a sync connection when using sync() with a string argument',
       function () {
         var store = pouch.store();
 
-        var sync = store.sync('http://localhost:1234/sync/to');
+        var sync = store.sync('http://localhost:1234/remote/sync');
+
+        expect(sync).to.exist;
+        expect(store._sync).to.exist;
+        expect(sync).to.equal(store._sync);
+        expect(sync.canceled).to.equal(false);
+      });
+
+    it('will create a sync connection when using sync() with a store argument',
+      function () {
+        var store = pouch.store();
+
+        var syncStore = new PouchDb('sync', { db: require('memdown') }).store();
+
+        var sync = store.sync(syncStore);
+
+        expect(sync).to.exist;
+        expect(store._sync).to.exist;
+        expect(sync).to.equal(store._sync);
+        expect(sync.canceled).to.equal(false);
+      });
+
+    it('will create a sync connection when using sync() with a pouchdb argument',
+      function () {
+        var store = pouch.store();
+
+        var syncStore = new PouchDb('sync', { db: require('memdown') });
+
+        var sync = store.sync(syncStore);
 
         expect(sync).to.exist;
         expect(store._sync).to.exist;
@@ -39,7 +77,7 @@ describe('Testing shelfdb sync', function () {
     it('will create a sync connection when providing sync option on initialization',
       function () {
         var store = pouch.store({
-          sync: 'http://localhost:1234/sync/to'
+          sync: 'http://localhost:1234/remote/sync'
         });
 
         var sync = store.sync();
